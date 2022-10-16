d917b34b3a8b2ba7c9be649c1baeaa7cf3b7d15c
RIYAS <riyass973@gmail.com>
2022-10-16 20:51:36 +0530

    first day

---
+++ index.js
@@ -0,0 +1,12 @@
+let typed = new Typed(".chng",{
+    strings: ['Back-End Developer'],
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true
+})
+let type = new Typed(".chg",{
+    strings: ['Back-End Developer'],
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true
+})
