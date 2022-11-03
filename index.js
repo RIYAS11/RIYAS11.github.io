142cdbc7ae9b497843901ebafa7e99a3dd7736b4
RIYAS <riyass973@gmail.com>
2022-11-03 12:57:44 +0530

    FINAL-PORTFOLIO

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-let typed = new Typed(".chng",{
-    strings: ['Back-End Developer'],
-    typeSpeed:100,
-    backSpeed:100,
-    loop:true
+
+let open = document.querySelector(".open");
+
+open.addEventListener("click", () => {
+
+    document.querySelector('.x').style.display = "block";
+    document.querySelector('.open').style.display = "none";
+    document.querySelector('.close').style.display = "block";
+ let url =   document.querySelectorAll('.qualification__line');
+  url.forEach(el => el.style.display="none");
 })
-let type = new Typed(".chg",{
-    strings: ['Back-End Developer'],
-    typeSpeed:100,
-    backSpeed:100,
-    loop:true
+
+
+let close = document.querySelector(".close");
+
+close.addEventListener("click", () => {
+
+    document.querySelector('.x').style.display = "none";
+    document.querySelector('.open').style.display = "block";
+    document.querySelector('.close').style.display = "none";
+    let url =   document.querySelectorAll('.qualification__line');
+  url.forEach(el => el.style.display="block");
 })
+
+
+
+
+
+let type = new Typed(".chg", {
+    strings: ['  I have knowledge in Java , Spring Boot , Hibernate , SQL , AWS.'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+})
+
+
