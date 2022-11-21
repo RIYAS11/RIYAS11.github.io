7c2f536ce5bed6a538dbfcaf40c3867b6fd06d6f
RIYAS <riyass973@gmail.com>
2022-11-21 22:38:37 +0530

    dl

--- index.js
+++ index.js
@@ -37,17 +37,14 @@ cont.forEach(el => {
       url.forEach(el => el.style.display = "block");
       flag = false;
 
-      // console.log("lop");
+  
     };
   })
 
 })
 
 
-// let pgit1 = document.querySelector(".pgit1");
-// pgit1.addEventListener("click",()=>{
-//   window.location.href="https://github.com/SohamDoshi/can-water-5067";
-// })
+
 
 
 
@@ -68,26 +65,7 @@ GitHubCalendar(".calendar", "riyas11", {
   tooltips: true,
 });
 
-// (".counting").each(function () {
-//   var $this = $(this),
-//     countTo = $this.attr("data-count");
 
-//   ({ countNum: $this.text() }).animate(
-//     {
-//       countNum: countTo,
-//     },
-//     {
-//       duration: 3000,
-//       easing: "linear",
-//       step: function () {
-//         $this.text(Math.floor(this.countNum));
-//       },
-//       complete: function () {
-//         $this.text(this.countNum);
-//       },
-//     }
-//   );
-// });
 
 
 
@@ -100,54 +78,6 @@ document.getElementById("git").addEventListener("click", () => {
   window.location.href = "https://github.com/RIYAS11/";
 })
 
-
-
-
-
-
-// document.getElementById("contact-form").addEventListener("submit", (e) => {
-//   e.preventDefault();
-
-//   //button animation
-//   document.getElementById("email-submit").innerText = "Sending...";
-
-//   if (
-//     document.getElementById("name").value != "" &&
-//     document.getElementById("email").value != "" &&
-//     document.getElementById("message").value != ""
-//   ) {
-//     let params = {
-//       name: document.getElementById("name").value,
-//       email: document.getElementById("email").value,
-//       message: document.getElementById("message").value,
-//     };
-
-//     const serviceID = "service_embce24";
-//     const templateID = "template_cci0niw";
-
-//     emailjs
-//       .send(serviceID, templateID, params)
-//       .then((res) => {
-//         document.getElementById("name").value = "";
-//         document.getElementById("email").value = "";
-//         document.getElementById("message").value = "";
-
-//         document.getElementById("email-submit").innerHTML = `
-//           Send message
-//           <i class="uil uil-message button__icon"></i>`;
-
-//         alert("Your message send susscessfully!");
-//       })
-//       .catch((err) => {
-//         document.getElementById("email-submit").innerHTML = `
-//           Send message
-//           <i class="uil uil-message button__icon"></i>`;
-//         console.log(err);
-//       });
-//   } else {
-//     alert("Please fill out all the fields");
-//   }
-// });
 
 
 
